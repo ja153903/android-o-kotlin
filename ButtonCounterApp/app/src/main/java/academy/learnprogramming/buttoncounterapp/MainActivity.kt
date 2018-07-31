6ffa1a0eff5c765a489b4e41a3e22aa28551e77f
jaimeabbariao <jaimeabbariao@gmail.com>
2018-07-31 00:57:28 -0400

    Added scrolling view

--- ButtonCounterApp/app/src/main/java/academy/learnprogramming/buttoncounterapp/MainActivity.kt
+++ ButtonCounterApp/app/src/main/java/academy/learnprogramming/buttoncounterapp/MainActivity.kt
@@ -2,6 +2,7 @@ package academy.learnprogramming.buttoncounterapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -20,12 +21,19 @@ class MainActivity : AppCompatActivity() {
         userInput = findViewById(R.id.editText)
         button = findViewById(R.id.button)
         textView = findViewById(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
 
         // If not null, then do this
         button?.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 numTimesClicked++
-                textView?.append("\nThe button got tapped $numTimesClicked time(s)")
+                textView?.append("The button got tapped $numTimesClicked time")
+                if (numTimesClicked != 1) {
+                    textView?.append("s\n")
+                } else {
+                    textView?.append("\n")
+                }
             }
         })
     }
