59a826f4f7673242cc1973576319063099bc5e33
jaimeabbariao <jaimeabbariao@gmail.com>
2018-08-03 16:46:54 -0400

    Completed project application

--- ButtonCounterApp/app/src/main/java/academy/learnprogramming/buttoncounterapp/MainActivity.kt
+++ ButtonCounterApp/app/src/main/java/academy/learnprogramming/buttoncounterapp/MainActivity.kt
@@ -3,38 +3,85 @@ package academy.learnprogramming.buttoncounterapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENTS = "TextContent"
+
 class MainActivity : AppCompatActivity() {
-    private var userInput: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-    private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "OnCreate: called")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById(R.id.editText)
-        button = findViewById(R.id.button)
+        val userInput: EditText = findViewById(R.id.editText)
+        val button: Button = findViewById(R.id.button)
+
         textView = findViewById(R.id.textView)
         textView?.text = ""
         textView?.movementMethod = ScrollingMovementMethod()
 
+        userInput.text.clear()
+
         // If not null, then do this
-        button?.setOnClickListener(object : View.OnClickListener {
+        button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                numTimesClicked++
-                textView?.append("The button got tapped $numTimesClicked time")
-                if (numTimesClicked != 1) {
-                    textView?.append("s\n")
-                } else {
-                    textView?.append("\n")
-                }
+                Log.d(TAG, "onClick: called")
+                // userInput.text is an Editable
+                textView?.append(userInput.text)
+                textView?.append("\n")
+//                userInput.text.clear()
+                userInput.setText("")
             }
         })
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart: called")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onRestoreInstanceState: called")
+        super.onRestoreInstanceState(savedInstanceState)
+        textView?.text = savedInstanceState?.getString(TEXT_CONTENTS, "")
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume: called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause: called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        Log.d(TAG, "onSaveInstanceState: called")
+        super.onSaveInstanceState(outState)
+        outState?.putString(TEXT_CONTENTS, textView?.text.toString())
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop: called")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: called")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart: called")
+        super.onRestart()
     }
 }
